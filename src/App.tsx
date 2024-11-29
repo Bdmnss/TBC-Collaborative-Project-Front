497712cb6023fe62ebb69327be8c76b2f8a458d9
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2024-11-29 18:34:55 +0400

    Add navigation and pages for Home, Login, and Register
    - Created Home, Login, and Register components in the pages folder
    - Set up routing in App.tsx using react-router-dom
    - Added navigation links to Home, Login, and Register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
