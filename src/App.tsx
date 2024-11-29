b66fc18ecf4af8db2bc38cb41adf62c884469ded
Elene <ellebuto@gmail.com>
2024-11-29 19:01:50 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { HomeLayout } from './pages/home';
+import { LogIn } from './pages/logIn';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomeLayout />} />
+      <Route path="login" element={<LogIn />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/logIn/index.tsx
@@ -0,0 +1,5 @@
+import { LoginCard } from './components/LoginCard';
+
+export const LogIn = () => {
+  return <LoginCard />;
+};
