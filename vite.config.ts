88db150689670b4a20c60f2cbbd8435abd8ff12e
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2024-11-29 19:43:03 +0400

    fix not dound error

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { dirname, resolve } from 'path';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(dirname(new URL(import.meta.url).pathname), 'src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
